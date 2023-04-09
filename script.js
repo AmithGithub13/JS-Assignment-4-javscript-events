8e47926397691fba280eb6c18429e18af65d4c07
Amith K <amithkadagodu1305@gmail.com>
2023-04-09 22:31:21 +0530

    Committed

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var img0="./Assets/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg";
+var img1="./Assets/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg";
+var img2="./Assets/c7e58861-3431-4189-9903-9880f5eebd181541402833566-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-3.jpg";
+var img3="./Assets/66490b64-32de-44b4-a6e4-fe36f1c040051541402833548-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-4.jpg";
+var img4="./Assets/957be784-7c5d-4e90-ab9f-0928015b22891541402833645-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-5.jpg";
+
+
+function changeImage1(){
+    productImg.src=img0;
+}
+function changeImage2(){
+    productImg.src=img1;
+}
+function changeImage3(){
+    productImg.src=img2;
+}
+function changeImage4(){
+    productImg.src=img3;
+}
+function changeImage5(){
+    productImg.src=img4;
+}
